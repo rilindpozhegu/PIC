7dc306973d8f242d691222d0a79f73175f58945b
Rilind Pozhegu <Rilind Pozhegu>
2016-11-08 11:10:48 +0100

    first commit of pci

---
+++ js/pic.js
@@ -0,0 +1,10 @@
+       // Galery Sponsors Comapny Page
+        $(document).ready(function() {
+            $("#owl-demo").owlCarousel({
+                autoPlay: 5000,
+                items: 6,
+                itemsDesktop: [1199, 3],
+                itemsDesktopSmall: [979, 3]
+            });
+
+        });
